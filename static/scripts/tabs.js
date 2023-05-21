dbddbeb65549dc09d6f9d3a65537b408369c4c25
Christian <elcman965@gmail.com>
2023-05-20 20:42:30 -0500

    todays amazing changes!!!!!

--- static/scripts/tabs.js
+++ static/scripts/tabs.js
@@ -1,18 +1,19 @@
+window.onload = (event) => {
+    setTimeout(function () {
+        document.getElementById("full-time").classList.remove("prevent-animation");
+    }, 200);
+};
+
 function openTab(evt, type) {
     var tabcontent = document.getElementsByClassName("tabcontent");
     var tablinks = document.getElementsByClassName("tablinks");
 
-    if (evt.currentTarget.classList.contains("active")) {
-        document.getElementById(type).style.display = "none";
-        evt.currentTarget.classList.remove("active");
-    } else {
-        for (var i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-        for (var i = 0; i < tablinks.length; i++) {
-            tablinks[i].classList.remove("active");
-        }
-        document.getElementById(type).style.display = "block";
-        evt.currentTarget.classList.add("active");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(type).style.display = "block";
+    evt.currentTarget.classList.add("active");
 }
